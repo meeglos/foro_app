0931223aea78a584ef2c8a763486392c88555452
meeglos <miguel@rodriguez.me>
2017-02-18 22:12:58 +0100

    Creating a post with slug - Unit Testing

---
+++ tests/unit/PostModelTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Post;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PostModelTest extends TestCase
+{
+    function test_adding_a_title_generates_a_slug()
+    {
+        $post = new Post([
+            'title' => 'Como instalar Laravel'
+        ]);
+
+        $this->assertSame('como-instalar-laravel', $post->slug);
+    }
+
+    function test_editing_the_title_changes_the_slug()
+    {
+        $post = new Post([
+            'title' => 'Como instalar Laravel'
+        ]);
+
+        $post->title = 'Como instalar Laravel 5.1 LTS';
+
+        $this->assertSame('como-instalar-laravel-51-lts', $post->slug);
+    }
+}
